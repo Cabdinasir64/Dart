9e666651c043ef04055e72000634f049e9e6171f
Cabdinasir64 <cabdinasira64@gmail.com>
2025-12-14 09:36:17 +0300

    feat: add example1.dart and example2.dart demonstrating variable usage

---
+++ variables/example2.dart
@@ -0,0 +1,21 @@
+void main(){
+  String name = "Ali";
+  print("Hello, $name!");
+  name = "Veli";
+  print("Hello, $name!");
+
+  int age = 25;
+  print("You are $age years old.");
+  age = 30;
+  print("You are now $age years old.");
+
+  bool isStudent = true;
+  print("Is student: $isStudent");
+  isStudent = false;
+  print("Is student: $isStudent");
+
+  double height = 5;
+  print("Height: $height"); 
+  height = 6.1;
+  print("Height: $height");
+}
